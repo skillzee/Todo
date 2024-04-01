a315a4530517499cd73d86c8e0b44acf41be2a16
Parth Sharma <112481963+skillzee@users.noreply.github.com>
2024-04-01 21:40:14 +0530

    com

--- Data/database.js
+++ Data/database.js
@@ -4,8 +4,8 @@ import mongoose from "mongoose";
 export const dbConnect = () =>{
     mongoose.connect(process.env.MONGO_URI,{
     dbName: "backendApi",
-}).then(()=>{
-    console.log("Database connected");
+}).then((c)=>{
+    console.log(`Database connected with ${c.connection.host}`);
 }).catch((e)=>{
     console.log(e);
 })
